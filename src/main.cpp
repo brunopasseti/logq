ae98b47538b0d671d214a79bc674a463fb985331
Bruno Passeti <brunopassetidev@gmail.com>
2022-03-11 01:28:57 -0300

    Começando implementação do modelo.
    Iria fazer direto usando glpk, mas sabendo que sua fama não é das melhores, decidi então
    implementar usando da interface entre solvers feito pela Coin-OR chamada de OSI.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <iomanip>
 #include <nlohmann/json.hpp>
 #include <glpk.h>
-
+#include <OsiGlpkSolverInterface.hpp>
+#include <OsiSolverInterface.hpp>
+#include <CoinPackedMatrix.hpp>
+#include <CoinPackedVector.hpp>
 // using json = nlohmann::json;
 
 typedef struct Data{
@@ -15,6 +18,8 @@ typedef struct Data{
     int omega;
     // Mapa aluno para indice;
     std::map<std::string, size_t> map_student_to_index_i;
+
+    int n_alunos, n_dias, n_horarios;
     // // Mapa dia da semana para index;
     // std::map<std::string, size_t> map_day_to_index_j;
 } Data;
@@ -33,12 +38,13 @@ Data parse_instance(std::string input){
         }
         fp >> n_alunos >> omega >> n_dias >> n_horarios;
         Data data{};
-        data.tau_i = std::vector<int>(std::atoi(n_alunos.data()), 0);
-        data.mi_i_j_k = std::vector<std::vector<std::vector<int>>>(std::atoi(n_alunos.data()), std::vector<std::vector<int>>(std::atoi(n_dias.data()), std::vector<int>(std::atoi(n_alunos.data()), 0)));
+        data.n_alunos = std::atoi(n_alunos.data());
+        data.n_dias = std::atoi(n_dias.data());
+        data.n_horarios = std::atoi(n_horarios.data());
+        data.tau_i = std::vector<int>(data.n_alunos, 0);
+        data.mi_i_j_k = std::vector<std::vector<std::vector<int>>>(data.n_alunos, std::vector<std::vector<int>>(data.n_dias, std::vector<int>(data.n_horarios, 0)));
         std::cout << n_alunos << omega << n_dias << n_horarios << std::endl;
         data.omega = std::atoi(omega.data());
-        size_t t_ndias = std::atoi(n_dias.data());
-        size_t t_horarios = std::atoi(n_horarios.data());
         ch = fp.peek();
         while( ch == '#' || ch == '\n' || ch == ' '){
             fp.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -53,11 +59,11 @@ Data parse_instance(std::string input){
             fp.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             ch = fp.peek();
         }
-        for(size_t i = 0; i < data.tau_i.size(); i++){
+        for(size_t i = 0; i < data.n_alunos; i++){
             fp >> tmp;
             data.map_student_to_index_i.emplace(tmp, i);
-            for(size_t j = 0; j < t_ndias; j++){
-                for(size_t k = 0; k < t_horarios; k++){
+            for(size_t j = 0; j < data.n_dias; j++){
+                for(size_t k = 0; k < data.n_horarios; k++){
                     fp >> tmp;
                     data.mi_i_j_k[i][j][k] = std::atoi(tmp.data());
                 }
@@ -71,7 +77,33 @@ int main(int argc, char** argv){
         std::cerr << "Error: missing instance path. Argc = " << argc << "." << std::endl;
         return EXIT_FAILURE;
     }
-        
+    OsiSolverInterface *problem = new OsiGlpkSolverInterface();
     Data instance = parse_instance(std::string(argv[argc-1]));
+    /*
+    /   MAX x_0_0_0 + x_0_0_1 + ....
+    /
+    /
+    */
+    int n_cols = instance.n_alunos*instance.n_dias*instance.n_horarios;
+    double * objective = new double[n_cols];//the objective coefficients
+    double * col_lb = new double[n_cols];//the column lower bounds
+    double * col_ub = new double[n_cols];//the column upper bounds
+    for(int i = 0; i < n_cols; i++){
+        objective[i] = 1.0;
+        col_lb[i] = 0.0;
+        col_ub[i] = 1.0;
+    }
+    int n_rows = n_cols;
+    CoinPackedMatrix * matrix = new CoinPackedMatrix(false,0,0);
+    matrix->setDimensions(0, n_cols);
+    for(int i = 0; i < instance.n_alunos; i++){
+        for(int j = 0; j < instance.n_dias; j++){
+            for(int k = 0; k < instance.n_horarios; k++){  
+                int index = i + j*instance.n_alunos + k*instance.n_alunos*instance.n_dias;
+                
+            }
+        }
+    }
+    delete problem;
     return EXIT_SUCCESS;
 }   
